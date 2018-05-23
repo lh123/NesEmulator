f930693336d249ba33f18669a040bebe74eefabc
lh123 <1585086582@qq.com>
2018-05-23 16:22:20 +0800

    优化构建脚本

--- cmake/FindGLFW.cmake
+++ cmake/FindGLFW.cmake
@@ -1,21 +1,37 @@
-set(GLFW_ROOT_DIR "${GLFW_ROOT_DIR}" CACHE PATH "Directory to search")
+set(GLFW_ROOT_DIR ${GLFW_ROOT_DIR} CACHE PATH "Directory to search")
 
-find_library(GLFW_LIBRARY
-    NAMES "libglfw3.dll.a" "libglfw3.dll" "glfw3.dll"
-    PATHS ${GLFW_ROOT_DIR}
-    PATH_SUFFIXES "lib-mingw-w64")
+if(NOT DEFINED GLFW_USE_STATIC_LIBS)
+    set(GLFW_USE_STATIC_LIBS ON 
+    CACHE BOOL 
+    "Set to ON to force the use of the static")
+endif()
 
-find_library(GLFW_STATIC_LIBRARY
-    NAMES "libglfw3.a"
-    PATHS ${GLFW_ROOT_DIR}
-    PATH_SUFFIXES "lib-mingw-w64")
+if(GLFW_USE_STATIC_LIBS)
+    set(GLFW_LIB_NAME "libglfw3.a")
+else()
+    set(GLFW_LIB_NAME "libglfw3dll.a")
+endif()
 
 find_path(GLFW_INCLUDE_DIR
     NAMES GLFW/glfw3.h
-    PATHS ${GLFW_ROOT_DIR}
-    PATH_SUFFIXES "include")
+    PATHS "${GLFW_ROOT_DIR}/include")
 
-find_file(GLFW_RUNTIME_LIBRARY
-    NAMES "glfw3.dll"
-    PATHS ${GLFW_ROOT_DIR}
-    PATH_SUFFIXES "lib-mingw-w64")
+find_library(GLFW_LIBRARY
+    NAMES ${GLFW_LIB_NAME}
+    PATHS "${GLFW_ROOT_DIR}/lib-mingw-w64")
+
+if(NOT GLFW_USE_STATIC_LIBS)
+    find_file(GLFW_RUNTIME_LIBRARY
+        NAMES "glfw3.dll"
+        PATHS "${GLFW_ROOT_DIR}/lib-mingw-w64")
+endif()
+
+if(GLFW_INCLUDE_DIR AND GLFW_LIBRARY)
+    set(GLFW_FOUND ON)
+else()
+    set(GLFW_FOUND OFF)
+endif()
+
+if(NOT GLFW_USE_STATIC_LIBS AND NOT GLFW_RUNTIME_LIBRARY)
+    set(GLFW_FOUND OFF)
+endif()
--- cmake/FindSDL2.cmake
+++ cmake/FindSDL2.cmake
@@ -1,21 +1,38 @@
-set(SDL2_ROOT_DIR "${SDL2_ROOT_DIR}" CACHE PATH "Directory to search")
+set(SDL2_ROOT_DIR ${SDL2_ROOT_DIR} CACHE PATH "Directory to search")
 
-find_library(SDL2_LIBRARY
-    NAMES "libSDL2.dll.a"
-    PATHS ${SDL2_ROOT_DIR}
-    PATH_SUFFIXES "lib")
+if(NOT DEFINED SDL2_USE_STATIC_LIBS)
+    set(SDL2_USE_STATIC_LIBS TRUE 
+    CACHE BOOL 
+    "Set to ON to force the use of the static")
+endif()
 
-find_library(SDL2_STATIC_LIBRARY
-    NAMES "libSDL2.a"
-    PATHS ${SDL2_ROOT_DIR}
-    PATH_SUFFIXES "lib")
+if(SDL2_USE_STATIC_LIBS)
+    set(SDL2_LIB_NAME "libSDL2.a")
+    set(SDL2_LINK_LIBRARY "Imm32" "Version" "Winmm")
+else()
+    set(SDL2_LIB_NAME "libSDL2.dll.a")
+endif()
 
 find_path(SDL2_INCLUDE_DIR
     NAMES SDL2/SDL.h
-    PATHS ${SDL2_ROOT_DIR}
-    PATH_SUFFIXES "include")
+    PATHS "${SDL2_ROOT_DIR}/include")
 
-find_file(SDL2_RUNTIME_LIBRARY
-    NAMES SDL2.dll
-    PATHS ${SDL2_ROOT_DIR}
-    PATH_SUFFIXES "bin")
+find_library(SDL2_LIBRARY
+    NAMES ${SDL2_LIB_NAME}
+    PATHS "${SDL2_ROOT_DIR}/lib")
+
+if(NOT SDL2_USE_STATIC_LIBS)
+    find_file(SDL2_RUNTIME_LIBRARY
+        NAMES "SDL2.dll"
+        PATHS "${SDL2_ROOT_DIR}/bin")
+endif()
+
+if(SDL2_INCLUDE_DIR AND SDL2_LIBRARY)
+    set(SDL2_FOUND TRUE)
+else()
+    set(SDL2_FOUND FALSE)
+endif()
+
+if(NOT SDL2_USE_STATIC_LIBS AND NOT SDL2_RUNTIME_LIBRARY)
+    set(SDL2_FOUND FALSE)
+endif()
